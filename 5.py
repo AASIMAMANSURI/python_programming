29bd48f6195c1ad1c75b422685d4da322705c995
AASIMAMANSURI <136147114+AASIMAMANSURI@users.noreply.github.com>
2024-02-12 12:08:02 +0530

    Casting

---
+++ 5.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#casting
+
+a= str(10)
+print(a)
+print(type(a))
+
+a = float(10)
+print(a)
+print(type(a))
+
+b = float(1000)
+print(b)
+print(type(b))
